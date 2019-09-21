f46e3088824ef6b58bcb3698e4da4f5872882dbc
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2019-09-22 01:58:54 +0200

    txt_to_csv now uses the new get_load_functions_and_paths function.

--- txt_to_csv.py
+++ txt_to_csv.py
@@ -6,20 +6,14 @@ import os
 if not os.path.exists("data_csv"):
     os.makedirs("data_csv")
 
-# Setting up a link between each txt file function and their equivalent csv file.
-txt_to_csv = [(datareader_txt.load_impersonation_2, "data_csv/170907_impersonation.csv"),
-              (datareader_txt.load_impersonation_3, "data_csv/170907_impersonation_2.csv"),
-              (datareader_txt.load_attack_free1, "data_csv/Attack_free_dataset.csv"),
-              (datareader_txt.load_attack_free2, "data_csv/Attack_free_dataset2.csv"),
-              (datareader_txt.load_dos, "data_csv/DoS_attack_dataset.csv"),
-              (datareader_txt.load_fuzzy, "data_csv/Fuzzy_attack_dataset.csv"),
-              (datareader_txt.load_impersonation_1, "data_csv/Impersonation_attack_dataset.csv")]
+# Getting a list of links between each txt file function and their equivalent csv file.
+load_functions_and_paths = datareader_txt.get_load_functions_and_paths()
 
 # Going through each link between txt file functions and csv files.
-for i in range(len(txt_to_csv)):
-    text_file = txt_to_csv[i][0](mode="txt_to_csv")
+for i in range(len(load_functions_and_paths)):
+    text_file = load_functions_and_paths[i][0](mode="txt_to_csv")
 
-    with open(txt_to_csv[i][1], "w", newline="") as csv_file:
+    with open(load_functions_and_paths[i][1], "w", newline="") as csv_file:
         csv_writer = csv.writer(csv_file, quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
         csv_writer.writerow(["timestamp", "id", "add", "dlc", "data"])
