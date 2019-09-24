61a11bc58e2837c50b4f649c8ec03f20e774fdef
Thomas-Herrmann <48517973+Thomas-Herrmann@users.noreply.github.com>
2019-09-24 13:24:50 +0200

    Created training, validation and test datasets

--- id_based_datasets.py
+++ id_based_datasets.py
@@ -110,6 +110,8 @@ def messages_to_idpoints(messages, period_ms, is_injected):
     return idpoints
 
 
+# writes input IDPoint to file,
+# using the input writer.
 def write_idpoint_csv(idpoint, datafile_writer):
     datafile_writer.writerow([
         str(idpoint.time_ms), str(idpoint.is_injected), str(idpoint.mean_id_interval),
@@ -117,16 +119,17 @@ def write_idpoint_csv(idpoint, datafile_writer):
         str(idpoint.num_ids), str(idpoint.num_msgs)])
 
 
-def write_idpoints_csv(idpoints, period_ms):
+# writes a list of IDPoints to file.
+def write_idpoints_csv(idpoints, period_ms, name):
     # Creating a csv path for the new file using the corresponding csv file currently loaded from.
     dir = "idpoint_dataset/"
-    csv_path = dir + str(len(idpoints)) + "_" + str(period_ms) + ".csv"
+    csv_path = dir + name + "_" + str(len(idpoints)) + "_" + str(period_ms) + "ms.csv"
 
     if not os.path.exists(dir):
         os.makedirs(dir)
 
     with open(csv_path, "w", newline="") as datafile:
-        datafile_writer = csv.writer(datafile, delimiter=",")
+        datafile_writer = csv.writer(datafile, delimiter=";")
 
         # Writing the header.
         datafile_writer.writerow([
@@ -137,7 +140,98 @@ def write_idpoints_csv(idpoints, period_ms):
             write_idpoint_csv(idpoint, datafile_writer)
 
 
-messages = datareader_csv.load_data("data_csv/Attack_free_dataset.csv", 0, 1000000)
-idpoints = messages_to_idpoints(messages, 100, False)
+# joins two lists of messages,
+# by offsetting the timestamps of messages in the second list.
+def concat_messages(msgs1, msgs2):
+    offset = msgs1[len(msgs1) - 1].timestamp
 
-write_idpoints_csv(idpoints, 100)
+    for msg in msgs2:
+        msg.timestamp += offset
+
+    return msgs1 + msgs2
+
+
+def neutralize_offset(messages):
+    offset = messages[0].timestamp
+
+    for message in messages:
+        message.timestamp -= offset
+
+    return messages
+
+
+def concat_idpoints(idpoints1, idpoints2):
+    offset = idpoints1[len(idpoints1) - 1].time_ms
+
+    for idpoint in idpoints2:
+        idpoint.time_ms += offset
+
+    return idpoints1 + idpoints2
+
+
+# returns a tuple containing:
+#   - a training set comprised of 70% of the data
+#   - a validation set comprised of 15% of the data
+#   - a test set comprised of 15% of the data
+def get_mixed_datasets(period_ms):
+    training, validation, test = [], [], []
+
+    attack_free_messages1 = neutralize_offset(datareader_csv.load_attack_free1())
+    attack_free_messages2 = neutralize_offset(datareader_csv.load_attack_free2())
+    dos_messages = neutralize_offset(datareader_csv.load_dos())
+    fuzzy_messages = neutralize_offset(datareader_csv.load_fuzzy())
+    imp_messages1 = neutralize_offset(datareader_csv.load_impersonation_1())
+    imp_messages2 = neutralize_offset(datareader_csv.load_impersonation_2())
+    imp_messages3 = neutralize_offset(datareader_csv.load_impersonation_3())
+
+    datasets = [
+        messages_to_idpoints(attack_free_messages1, period_ms, False),
+        messages_to_idpoints(attack_free_messages2, period_ms, False),
+        messages_to_idpoints(dos_messages, period_ms, True),
+        messages_to_idpoints(fuzzy_messages[0:450000], period_ms, False),
+        messages_to_idpoints(fuzzy_messages[450000:], period_ms, True),
+        messages_to_idpoints(imp_messages1[0:517000], period_ms, False),
+        messages_to_idpoints(imp_messages1[517000:], period_ms, True),
+        messages_to_idpoints(imp_messages2[0:330000], period_ms, False),
+        messages_to_idpoints(imp_messages2[330000:], period_ms, True),
+        messages_to_idpoints(imp_messages3[0:534000], period_ms, False),
+        messages_to_idpoints(imp_messages3[534000:], period_ms, True)]
+
+    for set in datasets:
+        training_high = math.floor(len(set) * 0.70)
+        validation_high = math.floor(len(set) * 0.85)
+
+        training += set[0:training_high]
+        validation += set[training_high:validation_high]
+        test += set[validation_high:]
+
+    return (training, validation, test)
+
+
+(training_set, validation_set, test_set) = get_mixed_datasets(100)
+
+write_idpoints_csv(training_set, 100, "mixed_training")
+write_idpoints_csv(validation_set, 100, "mixed_validation")
+write_idpoints_csv(test_set, 100, "mixed_test")
+
+
+# attack_free_messages = concat_messages(datareader_csv.load_attack_free1(), datareader_csv.load_attack_free2())
+# attack_free_idpoints = messages_to_idpoints(attack_free_messages, 100, False)
+# write_idpoints_csv(attack_free_idpoints, 100, "attack_free_full")
+
+# dos_idpoints = messages_to_idpoints(datareader_csv.load_dos(), 100, True)
+# write_idpoints_csv(dos_idpoints, 100, "dos_full")
+
+# fuzzy_idpoints = messages_to_idpoints(datareader_csv.load_fuzzy(0, 450000), 100, False) + messages_to_idpoints(datareader_csv.load_fuzzy(450000), 100, True)
+# write_idpoints_csv(fuzzy_idpoints, 100, "fuzzy_full")
+
+# imp_messages1 = neutralize_offset(datareader_csv.load_impersonation_1())
+# imp_idpoints1 = messages_to_idpoints(imp_messages1[0:517000], 100, False) + messages_to_idpoints(imp_messages1[517000:], 100, True)
+# imp_messages2 = neutralize_offset(datareader_csv.load_impersonation_2())
+# imp_idpoints2 = messages_to_idpoints(imp_messages2[0:330000], 100, False) + messages_to_idpoints(imp_messages2[330000:], 100, True)
+# imp_messages3 = neutralize_offset(datareader_csv.load_impersonation_3())
+# imp_idpoints3 = messages_to_idpoints(imp_messages3[0:534000], 100, False) + messages_to_idpoints(imp_messages3[534000:], 100, True)
+# imp_idpoints = concat_idpoints(concat_idpoints(imp_idpoints1, imp_idpoints2), imp_idpoints3)
+# write_idpoints_csv(imp_idpoints, 100, "impersonation_full")
+
+
