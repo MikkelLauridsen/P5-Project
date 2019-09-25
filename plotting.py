b27eb09cfffaa0b16ae38afdd50ec7c5f5f9d803
Spidercoder <Spidercoder@users.noreply.github.com>
2019-09-25 14:21:56 +0200

    Added plotting

---
+++ plotting.py
@@ -0,0 +1,43 @@
+import datareader_csv
+import matplotlib.pyplot as plt
+
+
+idpoints = datareader_csv.load_idpoints("idpoint_dataset/mixed_training_32888_100ms.csv", 0)
+
+time_ms = [idpoint.time_ms for idpoint in idpoints]
+mean_id_interval = [idpoint.mean_id_interval for idpoint in idpoints]
+variance_id_frequency = [idpoint.variance_id_frequency for idpoint in idpoints]
+num_id_transitions = [idpoint.num_id_transitions for idpoint in idpoints]
+num_ids = [idpoint.num_ids for idpoint in idpoints]
+num_msgs = [idpoint.num_msgs for idpoint in idpoints]
+
+
+plt.figure(figsize=(20, 10))
+plt.scatter(time_ms, mean_id_interval)
+plt.xlabel("Time")
+plt.ylabel("Median id interval")
+plt.show()
+
+plt.figure(figsize=(20, 10))
+plt.scatter(time_ms, variance_id_frequency)
+plt.xlabel("Time")
+plt.ylabel("Variance id frequency")
+plt.show()
+
+plt.figure(figsize=(20, 10))
+plt.scatter(time_ms, num_id_transitions)
+plt.xlabel("Time")
+plt.ylabel("# id transitions")
+plt.show()
+
+plt.figure(figsize=(20, 10))
+plt.scatter(time_ms, num_ids)
+plt.xlabel("Time")
+plt.ylabel("# ids")
+plt.show()
+
+plt.figure(figsize=(20, 10))
+plt.scatter(time_ms, num_msgs)
+plt.xlabel("Time")
+plt.ylabel("# messages")
+plt.show()
