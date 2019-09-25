39baf9d2b292094daf07262f73f776ddf59ec9a0
Spidercoder <Spidercoder@users.noreply.github.com>
2019-09-25 14:07:04 +0200

    Added ability to load idpoints to csv datareader

--- datareader_csv.py
+++ datareader_csv.py
@@ -1,19 +1,9 @@
 import csv
-from recordclass import dataobject
+from message import Message
+from idpoint import IDPoint
 
 
-class Message(dataobject):
-    timestamp: float
-    id: int
-    add: int
-    dlc: int
-    data: bytearray
-
-    def __str__(self):
-        return f"{{{self.timestamp}, {self.id}, {self.add}, {self.dlc}, {self.data}}}"
-
-
-def parse_message(row):
+def __parse_message(row):
     timestamp = float(row[0])
     id = int(row[1])
     add = int(row[2])
@@ -29,7 +19,19 @@ def parse_message(row):
     return Message(timestamp, id, add, dlc, data)
 
 
-def load_data(filepath, start=0, limit=None):
+def __parse_idpoint(row):
+    time_ms = float(row[0])
+    is_injected = bool(row[1])
+    mean_id_interval = float(row[2])
+    variance_id_frequency = float(row[3])
+    num_id_transitions = int(row[4])
+    num_ids = int(row[5])
+    num_msgs = int(row[6])
+
+    return IDPoint(time_ms, is_injected, mean_id_interval, variance_id_frequency, num_id_transitions, num_ids, num_msgs)
+
+
+def __load_data(filepath, parse_func, start, limit):
     data = []
     with open(filepath, "r") as f:
         csv_reader = csv.reader(f)
@@ -44,7 +46,7 @@ def load_data(filepath, start=0, limit=None):
         c = 0
         for row in csv_reader:
             if limit is None or c < limit:
-                data.append(parse_message(row))
+                data.append(parse_func(row))
                 c += 1
                 if c % 50000 == 0:
                     print(c)
@@ -55,39 +57,47 @@ def load_data(filepath, start=0, limit=None):
         return data
 
 
+def load_messages(filepath, start = 0, limit = None):
+    return __load_data(filepath, __parse_message, start, limit)
+
+
+def load_idpoints(filepath, start = 0, limit = None):
+    return __load_data(filepath, __parse_idpoint, start, limit)
+
+
 # Loads data from "Attack_free_dataset.txt"
 def load_attack_free1(start=0, limit=None):
-    return load_data("data_csv/Attack_free_dataset.csv", start, limit)
+    return load_messages("data_csv/Attack_free_dataset.csv", start, limit)
 
 
 # Loads data from "Attack_free_dataset2.txt"
 def load_attack_free2(start=0, limit=None):
-    return load_data("data_csv/Attack_free_dataset2.csv", start, limit)
+    return load_messages("data_csv/Attack_free_dataset2.csv", start, limit)
 
 
 # Loads data from "Impersonation_attack_dataset.txt"
 def load_impersonation_1(start=0, limit=None):
-    return load_data("data_csv/Impersonation_attack_dataset.csv", start, limit)
+    return load_messages("data_csv/Impersonation_attack_dataset.csv", start, limit)
 
 
 # Loads data from "170907_impersonation.txt"
 def load_impersonation_2(start=0, limit=None):
-    return load_data("data_csv/170907_impersonation.csv", start, limit)
+    return load_messages("data_csv/170907_impersonation.csv", start, limit)
 
 
 # Loads data from "170907_impersonation_2.txt"
 def load_impersonation_3(start=0, limit=None):
-    return load_data("data_csv/170907_impersonation_2.csv", start, limit)
+    return load_messages("data_csv/170907_impersonation_2.csv", start, limit)
 
 
 # Loads data from "DoS_attack_dataset.txt"
 def load_dos(start=0, limit=None):
-    return load_data("data_csv/DoS_attack_dataset.csv", start, limit)
+    return load_messages("data_csv/DoS_attack_dataset.csv", start, limit)
 
 
 # Loads data from "Fuzzy_attack_dataset.txt"
 def load_fuzzy(start=0, limit=None):
-    return load_data("data_csv/Fuzzy_attack_dataset.csv", start, limit)
+    return load_messages("data_csv/Fuzzy_attack_dataset.csv", start, limit)
 
 
 # Returning a list containing all the different load functions and corresponding paths in no particular order.
