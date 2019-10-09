87849f946c2ea8ee32bfcfd64f4ef42b8860aaf2
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2019-10-09 15:44:19 +0200

    Changed the svm model implementation so it uses an utility function.

--- models/svm.py
+++ models/svm.py
@@ -1,28 +1,11 @@
 import datareader_csv
 import idpoint
+import models.model_utility
 
 import sklearn
 
-idpoints = datareader_csv.load_idpoints("data/idpoint_dataset/mixed_training_32888_100ms.csv")
 
-# The label list that will contain the injected status for each idpoint.
-y = []
-
-# The instance list that will contain all the features for each instance.
-X = []
-
-# Going through each instance and extracting the features and labels.
-for instance in idpoints:
-    features = []
-    for attr in idpoint.idpoint_attributes:
-        if attr == "time_ms":
-            pass
-        elif attr == "is_injected":
-            y.append(instance.is_injected)
-        else:
-            features.append(getattr(instance, attr))
-
-    X.append(features)
+X, y = models.model_utility.split_feature_label()
 
 print(y[0])
 print(X[0])
