11436abc9559863aa6cf6e16f020fbf47aeb05c6
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2019-10-10 08:55:27 +0200

    Added base functionality for the svm implementation.

--- models/svm.py
+++ models/svm.py
@@ -4,8 +4,16 @@ import models.model_utility
 
 import sklearn
 
+training_filepath = "data/idpoint_dataset/mixed_training_135520_100ms.csv"
+test_filepath = "data/idpoint_dataset/mixed_test_29041_100ms.csv"
 
-X, y = models.model_utility.split_feature_label()
+X_train, y_train = models.model_utility.split_feature_label(datareader_csv.load_idpoints(training_filepath))
+X_test, y_test = models.model_utility.split_feature_label(datareader_csv.load_idpoints(test_filepath))
 
-print(y[0])
-print(X[0])
+X_train, X_test = models.model_utility.scale_features(X_train, X_test)
+
+print(y_train[0])
+print(X_train[0])
+
+print(y_test[0])
+print(X_test[0])
