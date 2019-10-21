8a71a1723c86e5c2e28de44ef56d5f4f5aa09ad6
Spidercoder <Spidercoder@users.noreply.github.com>
2019-10-21 13:57:52 +0200

    Cleaned up bayesian networks

--- models/bayesian_network.py
+++ models/bayesian_network.py
@@ -6,25 +6,31 @@ import models.model_utility as model_utility
 from sklearn.metrics import classification_report
 
 
+# Creates a hugin dataset from a list of sets of features and their labels
 def __feature_list_to_dataset(features_list, injected_labels, names):
     dataset = hugin.DataSet()
 
-    dataset.new_column("is_injected")
+    dataset.new_column("is_injected")  # Make sure is_injected is included in dataset
 
+    # Create one column per feature
     for name in names:
         dataset.new_column(name)
 
+    # Insert one row per set of features
     for row, features in enumerate(features_list):
         dataset.new_row()
 
+        # Insert is_injected separately
         dataset.set_data_item(row, 0, injected_labels[row])
 
+        # Insert features
         for col, value in enumerate(features):
             dataset.set_data_item(row, col + 1, str(value))
 
     return dataset
 
 
+# Creates a hugin dataset from a list of idpoints
 def __idpoints_to_dataset(idpoints):
     dataset = hugin.DataSet()
 
@@ -46,6 +52,7 @@ def __idpoints_to_dataset(idpoints):
     return dataset
 
 
+# Prints a hugin dataset. Used for debugging
 def __print_dataset(dataset, limit=0):
     num_cols = dataset.get_number_of_columns()
     num_rows = dataset.get_number_of_rows()
@@ -53,37 +60,30 @@ def __print_dataset(dataset, limit=0):
     if limit == 0:
         limit = num_rows
 
+    # Print columns
     for c in range(num_cols):
         col_name = dataset.get_column_name(c)
         print(f"{col_name}, ", end="")
     print()
 
+    # Print rows
     for r in range(num_rows):
         if r == limit:
             break
 
         for c in range(num_cols):
             value = dataset.get_data_item(r, c)
-            if c == 0:
+            if c == 0:  # c == 0 is the case for is_injected
                 print(value + ", ", end="")
             else:
                 print(f"%0.2f, " % float(value), end="")
         print()
 
 
+# Constructs the nodes of a network from a given dataset
 def __construct_network_nodes(domain, dataset):
-    # Add input nodes (one for each column in dataset)
-    for name_index in range(dataset.get_number_of_columns()):
-        name = dataset.get_column_name(name_index)
 
-        if name == "time_ms" or name == "is_injected":
-            continue
-
-        node = hugin.Node(domain, kind=hugin.KIND.CONTINUOUS)
-        node.set_name(name)
-        node.set_label(name)
-        node.get_experience_table()
-
+    # Insert node for is_injected. This is the only discrete node, and is therefore handled separately
     output_node = hugin.Node(domain, kind=hugin.KIND.DISCRETE)
     output_node.set_name("is_injected")
     output_node.set_label("is_injected")
@@ -94,17 +94,29 @@ def __construct_network_nodes(domain, dataset):
     output_node.set_state_label(3, "impersonation")
     output_node.get_experience_table()
 
+    # Add input nodes (one for each column in dataset). These are all continuous
+    for name_index in range(dataset.get_number_of_columns()):
+        name = dataset.get_column_name(name_index)
+
+        # Ignore time_ms and is_injected attributes
+        if name == "time_ms" or name == "is_injected":
+            continue
+
+        node = hugin.Node(domain, kind=hugin.KIND.CONTINUOUS)
+        node.set_name(name)
+        node.set_label(name)
+        node.get_experience_table()
+
 
 def __learn_structure_and_tables(domain, dataset):
     # Add learning data from dataset to domain
-    domain.add_cases(training_set, 0, training_set.get_number_of_rows())
+    domain.add_cases(dataset, 0, dataset.get_number_of_rows())
 
     # Learn structure and condition tables
     print("Learning structure...")
     domain.learn_structure()
     domain.compile()
     domain.save_to_memory()
-
     print("Learning tables...")
     domain.learn_tables()
 
@@ -112,23 +124,20 @@ def __learn_structure_and_tables(domain, dataset):
 def __get_predictions(domain, dataset):
     domain.set_number_of_cases(0)
     num_cases = dataset.get_number_of_rows()
+
+    # Add dataset cases to domain
     domain.add_cases(dataset, 0, num_cases)
 
     predictions = []
     for i in range(num_cases):
         domain.reset_inference_engine()
 
-        for j, attr in enumerate(idp.idpoint_attributes):
-            if attr == "time_ms" or attr == "is_injected":
-                continue
+        domain.enter_case(i)  # Enter evidence from dataset into network
 
-            value = float(dataset.get_data_item(i, j - 1))
-            domain.get_node_by_name(attr).enter_value(value)
-
-        #domain.enter_case(i)
         injected_node = domain.get_node_by_name("is_injected")
-        #injected_node.unset_case(i)
+        injected_node.retract_findings()  # Remove known evidence about the is_injected node
 
+        # Propagate evidence
         injected_node.get_junction_tree().propagate()
 
         normal_belief = injected_node.get_belief(0)
@@ -136,8 +145,8 @@ def __get_predictions(domain, dataset):
         dos_belief = injected_node.get_belief(2)
         impersonation_belief = injected_node.get_belief(3)
 
+        # Find state of is_injected node with largest value
         beliefs = [normal_belief, fuzzy_belief, dos_belief, impersonation_belief]
-
         largest_index = beliefs.index(max(beliefs))
 
         if largest_index == 0:
@@ -152,27 +161,35 @@ def __get_predictions(domain, dataset):
     return predictions
 
 
-if __name__ == "__main__":
-    os.chdir("..")
-
-    training_points = datareader_csv.load_idpoints("data/idpoint_dataset/mixed_training_37582_100ms.csv", 0)
-    test_points = datareader_csv.load_idpoints("data/idpoint_dataset/mixed_test_9396_100ms.csv", 0)
-
+# Trains a bayesian network and tests a dataset on it
+def train_and_predict(training_points, test_points):
     X_train, y_train = model_utility.split_feature_label(training_points)
     X_test, y_test = model_utility.split_feature_label(test_points)
 
+    # Create hugin datasets. names corresponds to the names of the features in training_points
     names = list(idp.idpoint_attributes).copy()
     names.remove("time_ms")
     names.remove("is_injected")
     training_set = __feature_list_to_dataset(X_train, y_train, names)
     test_set = __feature_list_to_dataset(X_test, y_test, names)
 
+    # Create hugin domain and train on training_set and predict on training_set
     domain = hugin.Domain()
-
     __construct_network_nodes(domain, training_set)
     __learn_structure_and_tables(domain, training_set)
 
-    #domain.save_as_kb("network.hkb")
+    domain.save_as_kb("bayesian_network.hkb")
 
-    predictions = __get_predictions(domain, test_set)
+    return __get_predictions(domain, test_set)
+
+
+if __name__ == "__main__":
+    os.chdir("..")
+
+    training_points = datareader_csv.load_idpoints("data/idpoint_dataset/mixed_training_37590_100ms.csv", 0)
+    test_points = datareader_csv.load_idpoints("data/idpoint_dataset/mixed_test_9398_100ms.csv", 0)
+    X_test, y_test = model_utility.split_feature_label(test_points)
+
+    predictions = train_and_predict(training_points, test_points)
+
     print(classification_report(y_test, predictions))
