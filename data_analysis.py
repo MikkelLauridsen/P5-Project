83337fc7ee0acb1b137ce381aa7b07d6a00e1ff6
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2019-10-29 13:05:04 +0100

    Added a function that centralizes the different information functions.

--- data_analysis.py
+++ data_analysis.py
@@ -3,13 +3,13 @@ import numpy as np
 import datareader_csv
 
 
-def get_mean_time_between_normal_messages(messages):
+def get_mean_time_between_normal_messages(messages, remote_frame_and_response_indices):
     """Returns the mean time between normal message neighbours, ignoring remote frames and remote frame responses."""
     # The times between two normal messages.
     times_between_messages = []
 
     # Changing the list to a set to improve the time complexity of the "in" operation.
-    remote_frame_and_response_indices = set(__get_remote_frame_and_response_indices(messages))
+    remote_frame_and_response_indices = set(remote_frame_and_response_indices)
 
     # Using the list of non-normal indices to find the time between all neighbour pairs of normal messages.
     for i in range(len(messages) - 1):
@@ -19,11 +19,9 @@ def get_mean_time_between_normal_messages(messages):
     return np.mean(times_between_messages)
 
 
-def get_mean_time_between_split_messages(messages):
+def get_mean_time_between_split_messages(messages, remote_frame_and_response_indices):
     """Returns the mean time between message pairs that have a remote frame or remote frame response between them."""
     times_between_messages = []
-
-    remote_frame_and_response_indices = __get_remote_frame_and_response_indices(messages)
 
     # Using the list of non-normal indices to find all neighbours of remote frames and remote frame responses.
     for index in remote_frame_and_response_indices:
@@ -55,11 +53,24 @@ def __get_remote_frame_and_response_indices(messages):
     return remote_frame_or_response_indices
 
 
+def analyze_messages(messages):
+    """Returns a dictionary where the keys are descriptions of the information in the values"""
+    remote_frame_and_response_indices = __get_remote_frame_and_response_indices(messages)
+
+    information = {
+        "Mean time between normal messages: ":
+            get_mean_time_between_normal_messages(messages, remote_frame_and_response_indices),
+        "Mean time between split messages: ":
+            get_mean_time_between_split_messages(messages, remote_frame_and_response_indices),
+    }
+
+    return information
+
+
 if __name__ == "__main__":
     file_messages = datareader_csv.load_attack_free1()
 
-    print(f"Average time between normal messages: {get_mean_time_between_normal_messages(file_messages)}")
-    print(f"Average time between split messages: {get_mean_time_between_split_messages(file_messages)}")
+    print(analyze_messages(file_messages))
 
     # Without removing remote frames: 0.00042314722179202243
     # Removing remote frames without closing the holes: 0.00047852379197671924
