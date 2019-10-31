03cd7e395464e47ea2ba425522e6107dd7cdaffd
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2019-10-31 19:31:03 +0100

    testing

--- models/svm.py
+++ models/svm.py
@@ -1,22 +1,27 @@
 from sklearn.svm import SVC
+import models.model_utility
+from sklearn.model_selection import GridSearchCV
 
 
-def svm():
-    return SVC(max_iter=1000)
+def svm(X_train, y_train):
+    # set up the parameter grid
+    print("Grid for SVM is now being set up")
+    param_grid = [
+        {'C': [0.01, 0.1, 1, 10, 50, 100], 'kernel': ['linear']},
+        {'C': [0.01, 0.1, 1, 10, 50, 100], 'gamma': [0.001, 0.0001], 'kernel': ['rbf', "poly", "sigmoid"]},
+    ]
+    print("Grid for SVM has now been set up, hyper parameters are being found")
+
+    # Find hyperparameters
+    grid_s = GridSearchCV(SVC(), param_grid, cv=5, n_jobs=-1, scoring="f1_macro")
+    grid_s.fit(X_train, y_train)
+
+    print(grid_s.best_estimator_)
+    print(grid_s.best_score_)
+    print(grid_s.best_params_)
 
 
-#def svm(X_train, y_train):
-#    # set up the parameter grid
-#    print("Grid for SVM is now being set up")
-#    param_grid = [
-#        {'C': [0.01, 0.1, 1, 10, 100], 'kernel': ['linear']},
-#        {'C': [0.01, 0.1, 1, 10, 100], 'gamma': [0.001, 0.0001], 'kernel': ['rbf', "poly", "sigmoid"]},
-#    ]
-#    print("Grid for SVM has now been set up, hyper parameters are being found")
-#
-#    #Find hyperparameters
-#    svm_model = best_hyper_parameters_for_all_model(SVC(), param_grid, X_train, y_train)
-#
-#    # Model gets send back to get accuracy taken
-#    print("The SVM model has now been created, and prediction of accuracy is now being calculated")
-#    return svm_model
+if __name__ == "__main__":
+    X_train, y_train = models.model_utility.get_standard_feature_split()
+
+    svm(X_train, y_train)
