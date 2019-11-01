377cd46b09b7f088f7d2f49b6110184b6a3765f1
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2019-11-01 11:07:50 +0100

    Added a way to find the knn hyperparameters for the standard case.

--- models/knn.py
+++ models/knn.py
@@ -1,25 +1,33 @@
 from sklearn.neighbors import KNeighborsClassifier
+import models.model_utility
+from sklearn.model_selection import GridSearchCV
 
 
 def knn():  # baseline parameters
     return KNeighborsClassifier()
 
 
-# def knn(X_train, y_train, length):
-#    # Calculate k
-#    print("The knn model is now being created and fitted with data")
-#    k = round(math.sqrt(length), 0)
-#    if k % 2 == 0:
-#        k = k - 1
-#    k = int(k)
-#
-#    # Define the model
-#    knn = KNeighborsClassifier(algorithm='auto', leaf_size=30, metric='euclidean', metric_params=None,
-#                               n_jobs=-1, n_neighbors=k, p=2, weights='distance')
-#
-#    #Fit the model
-#    knn.fit(X_train, y_train)
-#
-#    # Model send back for accuracy
-#    print("Knn model has been created, prediction of accuracy is now being calculated")
-#    return knn
+def knn_hyperparameter(X_train, y_train):
+    """Finding the best hyperparameters for knn based on given training data."""
+    # set up the parameter grid
+    param_grid = [
+        {
+            "n_neighbors": [1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144],
+            "weights": ["uniform", "distance"],
+            "metric": ["euclidean", "manhattan"]
+        }
+    ]
+
+    # Find hyperparameters
+    grid_s = GridSearchCV(KNeighborsClassifier(), param_grid, cv=5, scoring="f1_macro", n_jobs=-1, verbose=10)
+    grid_s.fit(X_train, y_train)
+
+    print(grid_s.best_estimator_)
+    print(grid_s.best_score_)
+    print(grid_s.best_params_)
+
+
+if __name__ == "__main__":
+    X_train, y_train = models.model_utility.get_standard_feature_split()
+
+    knn_hyperparameter(X_train, y_train)
