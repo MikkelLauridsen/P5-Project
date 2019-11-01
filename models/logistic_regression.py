e70d7282a688e6ba65bbfec0ae4a61d37a8c138c
Prussia <Tjijuni@gmail.com>
2019-11-01 08:00:03 +0100

    Logistic regression

--- models/logistic_regression.py
+++ models/logistic_regression.py
@@ -1,9 +1,34 @@
-from sklearn.linear_model import LogisticRegression
-
+import os
+import numpy as np
+from sklearn.linear_model import LogisticRegression, logistic
+import models.model_utility
+from sklearn.model_selection import GridSearchCV
 
 def lr():  # baseline parameters
     return LogisticRegression()
 
+
+def lgr(X_train, y_train):
+    # Create regularization penalty space
+    penalty = ['l1', 'l2']
+
+    # Create regularization hyperparameter space
+    C = np.logspace(0, 4, 10)
+
+    # Create hyperparameter options
+    hyperparameters = dict(C=C, penalty=penalty)
+
+    best_model = GridSearchCV(logistic(), hyperparameters, cv=5, scoring="f1_macro")
+    best_model.fit(X_train, y_train)
+    print('Best Penalty:', best_model.best_estimator_.get_params()['penalty'])
+    print('Best C:', best_model.best_estimator_.get_params()['C'])
+    print(best_model.best_params_)
+
+
+if __name__ == "__main__":
+    os.chdir("..")
+    X_train, y_train = models.model_utility.get_standard_feature_split()
+    lgr(X_train, y_train)
 
 #def logistic_regression(X_train, y_train):
 #    # Define the model
