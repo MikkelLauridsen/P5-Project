fb30be52e64b49d8a2f3db8e6bf3e08f08500e21
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2019-11-04 15:21:46 +0100

    Made the main in data manipulation a lot prettier.

--- data_manipulation.py
+++ data_manipulation.py
@@ -110,16 +110,11 @@ if __name__ == "__main__":
     if not os.path.exists(target_dir):
         os.makedirs(target_dir)
 
-    manipulate_dos_data_field(source_dir + "DoS_attack_dataset.csv", target_dir + "DoS_manipulated.csv")
+    manipulate_dos_data_field(f"{source_dir}DoS_attack_dataset.csv", f"{target_dir}DoS_manipulated.csv")
 
-    manipulate_remote_frames(source_dir + "Attack_free_dataset.csv", target_dir + "Attack_free_dataset.csv")
-    manipulate_remote_frames(source_dir + "Attack_free_dataset2.csv", target_dir + "Attack_free_dataset2.csv")
-    manipulate_remote_frames(source_dir + "DoS_attack_dataset.csv", target_dir + "DoS_attack_dataset.csv")
-    manipulate_remote_frames(source_dir + "Fuzzy_attack_dataset.csv", target_dir + "Fuzzy_attack_dataset.csv")
-    manipulate_remote_frames(source_dir + "170907_impersonation.csv", target_dir + "170907_impersonation.csv")
-    manipulate_remote_frames(source_dir + "170907_impersonation_2.csv", target_dir + "170907_impersonation_2.csv")
-    manipulate_remote_frames(source_dir + "Impersonation_attack_dataset.csv",
-                             target_dir + "Impersonation_attack_dataset.csv")
+    # Going through each file in the source dir.
+    for filename in os.listdir(source_dir):
+        manipulate_remote_frames(f"{source_dir}{filename}", f"{target_dir}{filename}")
 
-    manipulate_remote_frames("data/manipulated/DoS_manipulated.csv", target_dir + "DoS_manipulated.csv")
-    manipulate_dlc("data/manipulated/Attack_free_dataset2.csv", target_dir + "Attack_free_dataset2.csv", 8)
+    manipulate_remote_frames("data/manipulated/DoS_manipulated.csv", f"{target_dir}DoS_manipulated.csv")
+    manipulate_dlc("data/manipulated/Attack_free_dataset2.csv", f"{target_dir}Attack_free_dataset2.csv", 8)
