6f2a40b15dbcbac6725f6d5151f81371b24af71f
Nabilety <37532159+Nabilety@users.noreply.github.com>
2019-11-04 15:36:01 +0100

    Decision Tree hyperparameter gridsearch

--- models/decision_trees.py
+++ models/decision_trees.py
@@ -1,22 +1,31 @@
 from sklearn.tree import DecisionTreeClassifier
+import models.model_utility
+from sklearn.model_selection import GridSearchCV
 
 
-def dt(parameters):  # baseline parameters
-    return DecisionTreeClassifier(max_depth=9).set_params(**parameters)
+#def dt():  # baseline parameters
+#    return DecisionTreeClassifier(max_depth=9)
 
 
-#def decision_trees(X_train, y_train):
-#    # set up the parameter grid
-#    print("Grid for decision trees is now being set up")
-#    parameter_grid = [{'max_depth': [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
-#                       'min_samples_split': [2, 3, 5, 8],
-#                       'max_features': [None, 2, 3, 5],
-#                       'criterion': ['gini', 'entropy']}]
-#    print("Grid for decision trees has now been set up, hyper parameters are being found")
-#
-#    # Find hyper parameters
-#    decision_trees_model = best_hyper_parameters_for_all_model(DecisionTreeClassifier(), parameter_grid, X_train, y_train)
-#
-#    # returns the model to calculate the accuracy
-#    print("The decision tree model has now been created, and prediction of accuracy is now being calculated")
-#    return decision_trees_model
+def decision_trees(X_train, y_train):
+    # set up the parameter grid
+    print("Grid for decision trees is now being set up")
+    parameter_grid = [{'max_depth': [2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233],
+                       'min_samples_split': [2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233],
+                       'criterion': ['gini', 'entropy']}]
+    print("Grid for decision trees has now been set up, hyper parameters are being found")
+
+    # Find hyper parameters
+    #decision_trees_model = best_hyper_parameters_for_all_model(DecisionTreeClassifier(), parameter_grid, X_train, y_train)
+
+    grid_s = GridSearchCV(DecisionTreeClassifier(), parameter_grid, cv=5, scoring="f1_macro", verbose=10)
+    grid_s.fit(X_train, y_train)
+
+    print(grid_s.best_params_)
+    print(grid_s.best_score_)
+    # returns the model to calculate the accuracy
+    #print("The decision tree model has now been created, and prediction of accuracy is now being calculated")
+    #return decision_trees_model
+if __name__ == "__main__":
+    X_train, y_train = models.model_utility.get_standard_feature_split()
+    decision_trees(X_train, y_train)
