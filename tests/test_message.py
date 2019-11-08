b8ed0c12b6e4ebdcf9c8d26990b339dcf1fb6772
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2019-11-08 09:33:16 +0100

    Added full test coverage to the message.py file.

--- tests/test_message.py
+++ tests/test_message.py
@@ -6,7 +6,7 @@ from message import Message
 
 class TestMessage(TestCase):
     def setUp(self) -> None:
-        self.message_row = [0.000224, 809, 0, 8, "07 a7 7f 8c 11 2f 00 10"]
+        self.message_row = ['0.000224', 809, 0, 8, "07 a7 7f 8c 11 2f 00 10"]
 
         self.message_object = Message(0.000224, 809, 0, 8, bytearray(b'\x07\xa7\x7f\x8c\x11/\x00\x10'))
 
@@ -14,3 +14,8 @@ class TestMessage(TestCase):
         result = message.parse_csv_row(self.message_row)
 
         self.assertEqual(result, self.message_object)
+
+    def test_get_csv_row(self):
+        result = message.get_csv_row(self.message_object)
+
+        self.assertEqual(result, self.message_row)
