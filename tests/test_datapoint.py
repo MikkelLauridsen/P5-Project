259da718a3ad343f2a4d63093ad367b6ed9ba377
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2019-11-08 10:04:59 +0100

    Added full test coverage to the datapoint.py file.

--- tests/test_datapoint.py
+++ tests/test_datapoint.py
@@ -23,7 +23,8 @@ class TestDataPoint(TestCase):
         kurtosis_variance_data_bit_count_id = 15.457159141707956
         skewness_id_interval_variances = 0.694951701467612
 
-        self.data_point_row = [time_ms, class_label, mean_id_interval, variance_id_frequency, num_id_transitions, num_ids,
+        self.data_point_row = [time_ms, class_label, mean_id_interval, variance_id_frequency, num_id_transitions,
+                               num_ids,
                                num_msgs, mean_id_intervals_variance, mean_data_bit_count, variance_data_bit_count,
                                mean_variance_data_bit_count_id, kurtosis_id_interval, kurtosis_id_frequency,
                                skewness_id_frequency, kurtosis_mean_id_intervals, kurtosis_variance_data_bit_count_id,
@@ -36,6 +37,12 @@ class TestDataPoint(TestCase):
                                            kurtosis_id_frequency, skewness_id_frequency, kurtosis_mean_id_intervals,
                                            kurtosis_variance_data_bit_count_id, skewness_id_interval_variances)
 
+        self.header = ["time_ms", "class_label", "mean_id_interval", "variance_id_frequency", "num_id_transitions",
+                       "num_ids", "num_msgs", "mean_id_intervals_variance", "mean_data_bit_count",
+                       "variance_data_bit_count", "mean_variance_data_bit_count_id", "kurtosis_id_interval",
+                       "kurtosis_id_frequency", "skewness_id_frequency", "kurtosis_mean_id_intervals",
+                       "kurtosis_variance_data_bit_count_id", "skewness_id_interval_variances"]
+
     def test_parse_csv_row(self):
         result = datapoint.parse_csv_row(self.data_point_row)
 
@@ -45,3 +52,15 @@ class TestDataPoint(TestCase):
         result = datapoint.get_csv_row(self.data_point_object)
 
         self.assertEqual(result, self.data_point_row)
+
+    def test_is_header_matching_match(self):
+        diff, diff_list = datapoint.is_header_matching(self.header)
+        self.assertTrue(diff)
+        self.assertEqual(diff_list, set())
+
+    def test_is_header_matching_no_match(self):
+        header = []
+
+        diff, diff_list = datapoint.is_header_matching(header)
+        self.assertFalse(diff)
+        self.assertEqual(diff_list, set(self.header))
