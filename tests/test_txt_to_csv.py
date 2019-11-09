5594721cc0838de2b26c19b98dc70909cf19b847
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2019-11-09 16:28:40 +0100

    Made a test case for the txt_to_csv function, testing on 10 lines.

--- tests/test_txt_to_csv.py
+++ tests/test_txt_to_csv.py
@@ -2,6 +2,7 @@ from unittest import TestCase
 import os
 import txt_to_csv
 import shutil
+import pandas as pd
 
 
 class TestTxtToCsv(TestCase):
@@ -27,21 +28,28 @@ class TestTxtToCsv(TestCase):
 
         # Setting up a test txt file containing the first 10 rows in the file "Attack_free_dataset2.txt".
         with open("data/raw/Attack_free_dataset2.txt", "r") as reader:
-            reader.readline()
-            for i in range(10):
+            for i in range(11):
                 messages.append(reader.readline())
-
             with open("tests/txt/Attack_free_dataset2.txt", "w") as writer:
                 for message in messages:
                     writer.write(message)
 
+        self.attack_free_1_csv_expected = pd.read_csv("tests/csv_expected/Attack_free_dataset.csv").values.tolist()
+        self.attack_free_2_csv_expected = pd.read_csv("tests/csv_expected/Attack_free_dataset2.csv").values.tolist()
+
     def tearDown(self) -> None:
         # Deleting the files used in the tests.
-        delete_list = ["tests/txt/", "tests/csv/"]
+        delete_list = ["tests/txt/", "tests/csv_actual/"]
 
         for filepath in delete_list:
             if os.path.exists(filepath):
                 shutil.rmtree(filepath, ignore_errors=True)
 
     def test_txt_to_csv(self):
-        txt_to_csv.txt_to_csv("tests/txt/", "tests/csv/")
+        txt_to_csv.txt_to_csv("tests/txt/", "tests/csv_actual/")
+
+        attack_free_1_csv_actual = pd.read_csv("tests/csv_actual/Attack_free_dataset.csv").values.tolist()
+        attack_free_2_csv_actual = pd.read_csv("tests/csv_actual/Attack_free_dataset2.csv").values.tolist()
+
+        self.assertEqual(self.attack_free_1_csv_expected, attack_free_1_csv_actual)
+        self.assertEqual(self.attack_free_2_csv_expected, attack_free_2_csv_actual)
