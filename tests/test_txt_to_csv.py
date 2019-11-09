5939df63b4588db7d205569b776ce3446022c5b3
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2019-11-09 15:32:15 +0100

    Made the final setup for the txt_to_csv tests.

--- tests/test_txt_to_csv.py
+++ tests/test_txt_to_csv.py
@@ -1,10 +1,17 @@
 from unittest import TestCase
 import os
+import txt_to_csv
+import shutil
 
 
 class TestTxtToCsv(TestCase):
     def setUp(self) -> None:
         os.chdir("..")
+        txt_dir = "tests/txt/"
+
+        if not os.path.exists(txt_dir):
+            os.makedirs(txt_dir)
+
         messages = []
 
         # Setting up a test txt file containing the first 10 rows in the file "Attack_free_dataset.txt".
@@ -12,7 +19,7 @@ class TestTxtToCsv(TestCase):
             for i in range(10):
                 messages.append(reader.readline())
 
-            with open("tests/attack_free_1_test.txt", "w") as writer:
+            with open("tests/txt/Attack_free_dataset.txt", "w") as writer:
                 for message in messages:
                     writer.write(message)
 
@@ -24,15 +31,17 @@ class TestTxtToCsv(TestCase):
             for i in range(10):
                 messages.append(reader.readline())
 
-            with open("tests/attack_free_2_test.txt", "w") as writer:
+            with open("tests/txt/Attack_free_dataset2.txt", "w") as writer:
                 for message in messages:
                     writer.write(message)
 
     def tearDown(self) -> None:
         # Deleting the files used in the tests.
-        delete_list = ["tests/attack_free_1_test.txt", "tests/attack_free_2_test.txt",
-                       "tests/attack_free_1_test.csv", "tests/attack_free_2_test.csv"]
+        delete_list = ["tests/txt/", "tests/csv/"]
 
         for filepath in delete_list:
             if os.path.exists(filepath):
-                os.remove(filepath)
+                shutil.rmtree(filepath, ignore_errors=True)
+
+    def test_txt_to_csv(self):
+        txt_to_csv.txt_to_csv("tests/txt/", "tests/csv/")
