d2020fc95a69e2ebda9d8b87431021cb62d3fa80
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2019-11-14 10:53:19 +0100

    Changed the directory setup for the tests to contain each test with the resources it uses.

--- tests/test_txt_to_csv/test_txt_to_csv.py
+++ tests/test_txt_to_csv/test_txt_to_csv.py
@@ -7,8 +7,11 @@ import pandas as pd
 
 class TestTxtToCsv(TestCase):
     def setUp(self) -> None:
+        # Going to the main project folder by changing the working directory to the parent two times.
         os.chdir("..")
-        txt_dir = "tests/txt/"
+        os.chdir("..")
+
+        txt_dir = "tests/test_txt_to_csv/txt/"
 
         if not os.path.exists(txt_dir):
             os.makedirs(txt_dir)
@@ -20,7 +23,7 @@ class TestTxtToCsv(TestCase):
             for i in range(10):
                 messages.append(reader.readline())
 
-            with open("tests/txt/Attack_free_dataset.txt", "w") as writer:
+            with open("tests/test_txt_to_csv/txt/Attack_free_dataset.txt", "w") as writer:
                 for message in messages:
                     writer.write(message)
 
@@ -30,26 +33,30 @@ class TestTxtToCsv(TestCase):
         with open("data/raw/Attack_free_dataset2.txt", "r") as reader:
             for i in range(11):
                 messages.append(reader.readline())
-            with open("tests/txt/Attack_free_dataset2.txt", "w") as writer:
+            with open("tests/test_txt_to_csv/txt/Attack_free_dataset2.txt", "w") as writer:
                 for message in messages:
                     writer.write(message)
 
-        self.attack_free_1_csv_expected = pd.read_csv("tests/csv_expected/Attack_free_dataset.csv").values.tolist()
-        self.attack_free_2_csv_expected = pd.read_csv("tests/csv_expected/Attack_free_dataset2.csv").values.tolist()
+        self.attack_free_1_csv_expected = \
+            pd.read_csv("tests/test_txt_to_csv/csv_expected/Attack_free_dataset.csv").values.tolist()
+        self.attack_free_2_csv_expected = \
+            pd.read_csv("tests/test_txt_to_csv/csv_expected/Attack_free_dataset2.csv").values.tolist()
 
     def tearDown(self) -> None:
         # Deleting the files used in the tests.
-        delete_list = ["tests/txt/", "tests/csv_actual/"]
+        delete_list = ["tests/test_txt_to_csv/txt/", "tests/test_txt_to_csv/csv_actual/"]
 
         for filepath in delete_list:
             if os.path.exists(filepath):
                 shutil.rmtree(filepath, ignore_errors=True)
 
     def test_txt_to_csv(self):
-        txt_to_csv.txt_to_csv("tests/txt/", "tests/csv_actual/")
+        txt_to_csv.txt_to_csv("tests/test_txt_to_csv/txt/", "tests/test_txt_to_csv/csv_actual/")
 
-        attack_free_1_csv_actual = pd.read_csv("tests/csv_actual/Attack_free_dataset.csv").values.tolist()
-        attack_free_2_csv_actual = pd.read_csv("tests/csv_actual/Attack_free_dataset2.csv").values.tolist()
+        attack_free_1_csv_actual = \
+            pd.read_csv("tests/test_txt_to_csv/csv_actual/Attack_free_dataset.csv").values.tolist()
+        attack_free_2_csv_actual = \
+            pd.read_csv("tests/test_txt_to_csv/csv_actual/Attack_free_dataset2.csv").values.tolist()
 
         self.assertEqual(self.attack_free_1_csv_expected, attack_free_1_csv_actual)
         self.assertEqual(self.attack_free_2_csv_expected, attack_free_2_csv_actual)
