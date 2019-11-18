f1563173ae3d66efedab2682372f8f8dfe2fa6c1
Spidercoder <Spidercoder@users.noreply.github.com>
2019-11-18 15:05:08 +0100

    Added more docstrings

--- model_selection.py
+++ model_selection.py
@@ -4,13 +4,6 @@ from models.model_utility import get_scaled_test, get_scaled_training_validation
 from run_models import selected_models, create_and_save_results
 
 
-def __calculate_result_weight(result, f1_type='macro'):
-    x = result.times['feature_time'] / 1000000
-    y = result.times['model_time'] / 1000000
-    z = result.metrics[f1_type].f1
-    return x * -0.25 + y * -10 + z * 3.5
-
-
 def run_on_test(configurations):
     for configuration in configurations:
         X_test, y_test, feature_time_dict = get_scaled_test(
@@ -42,16 +35,34 @@ def run_on_test(configurations):
             is_test=True)
 
 
-def sort_by_score(results):
-    results.sort(key=__calculate_result_weight, reverse=True)
+def __calculate_result_weight(result, w_ft, w_mt, w_f1, f1_type):
+    x = result.times['feature_time'] / 1000000
+    y = result.times['model_time'] / 1000000
+    z = result.metrics[f1_type].f1
+    return x * w_ft + y * w_mt + z * w_f1
 
 
-def get_best_for_models(results, models):
+def __sort_by_score(results, w_ft, w_mt, w_f1, f1_type):
+    sort_func = lambda x: __calculate_result_weight(x, w_ft, w_mt, w_f1, f1_type)
+    results.sort(key=sort_func, reverse=True)
+
+
+def get_best_for_models(results, models, w_ft=-0.25, w_mt=-10, w_f1=3.5, f1_type='macro'):
+    """
+    Returns a list of the best results. One for each model
+    :param results: A list of results to search through
+    :param models: A list of model labels to find best models for
+    :param w_ft: The weight of feature time
+    :param w_mt: The weight of model time
+    :param w_f1: The weight of f1 score
+    :param f1_type: f1 type
+    :return:
+    """
     best_results = []
 
     for model in models:
         model_results = metrics.filter_results(results, models=[model])
-        sort_by_score(model_results)
+        __sort_by_score(model_results, w_ft, w_mt, w_f1, f1_type)
 
         best_results.append(model_results[0])
 
