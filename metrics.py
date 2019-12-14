efbbfa1bdcb49cb8ba039c5e91fefe23da563f88
Thomas-Herrmann <48517973+Thomas-Herrmann@users.noreply.github.com>
2019-12-14 18:12:28 +0100

    Fixed a bug

--- metrics.py
+++ metrics.py
@@ -202,6 +202,13 @@ def get_metrics(y_actual, y_predict):
     return metrics
 
 
+def load_result(window_ms, stride_ms, imp_split, dos_type, model, baseline, subset, is_test):
+    metrics = datareader_csv.load_metrics(window_ms, stride_ms, imp_split, dos_type, model, baseline, subset, is_test)
+    times = datareader_csv.load_times(window_ms, stride_ms, imp_split, dos_type, model, baseline, subset, is_test)
+
+    return Result(window_ms, stride_ms, model, imp_split, dos_type, baseline, subset, is_test, metrics, times)
+
+
 def get_result_path(result, is_test):
     return get_metrics_path(
         result.window_ms, result.stride_ms,
--- plotting/model_plotting.py
+++ plotting/model_plotting.py
@@ -668,8 +668,13 @@ if __name__ == '__main__':
     #plot_barchart_subsets(barchart_subsets_results, None, subsets, labels, title)
 
     # Bar plots
-    best_validation_results = model_selection.get_best_for_models(validation_results, conf.selected_models.keys(), 0, 0, 1, 'normal', True)
-    best_test_results = [datareader_csv.__load_result(metrics.get_result_path(result, True)) for result in best_validation_results]
+    best_validation_results = model_selection.get_best_for_models(validation_results, conf.selected_models.keys(), 0, 0, 1, 'normal', False)
+    best_test_results = [metrics.load_result(
+        result.window_ms, result.stride_ms,
+        result.imp_split, result.dos_type,
+        result.model, result.baseline,
+        result.subset, True) for result in best_validation_results]
+
     plot_types = ['f1', 'fpr', 'fnr', 'recall', 'precision', 'accuracy']
     metrics_types = ['macro', 'normal', 'impersonation', 'dos', 'fuzzy']
 
